dbcc8a71fe0c2b5bc32bc5891bea9c33ab9e230c
katerinanoir333 <katerina.noir333@gmail.com>
2026-07-18 20:20:40 -0500

    change endpoint of formspree

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Public form endpoint only. Recipient addresses belong in the provider dashboard, never here.
 window.THON_CONFIG = Object.freeze({
-  formEndpoint: "https://formspree.io/f/xbdnggkd"
+  formEndpoint: "https://formspree.io/f/xrenqyjo"
 });
